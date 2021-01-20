da5b05a33327a44dbf5217708749c32467cfb4b4
Clark19 <clarkgable1@gmail.com>
2021-01-21 00:00:01 +0900

    class practice

--- 모던 웹을 위한 JS + jQuery 입문 3판 실습/jsStudy.js
+++ 모던 웹을 위한 JS + jQuery 입문 3판 실습/jsStudy.js
@@ -51,11 +51,11 @@ let b = [20,21,22,23,24]
 let s1 = [...a, ...b]
 logA(s1); log('\n')
 
-// .concat() 이용, immutable - 자기 자신을 변경하지 않고, 새로운 배열 리턴.
+// .concat() 이용, immutable - 자기 자신을 변경하지 않고, 새로운 배열 리턴. - https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
 s2 = a.concat(b)
 logA(s2); log('\n')
 
-// .splice()와 ...(전개 연산자) 동시 이용, , mutable - 자기 자신을 변경 시킴.
+// .splice()와 ...(전개 연산자) 동시 이용, , mutable - 자기 자신을 변경 시킴. - https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 a.splice(a.length, 0, ...b)
 // !주의: a.splice(a.length, 0, b) 이렇게 사용하면 a[a.length]에 b배열이 원소 각각이 풀려서 들어가는게 아니라 배열 자체가 하나의 요소로 들어감.
 logA(a)
@@ -67,3 +67,23 @@ log(a[5]); log('\n')
 logA(a)
 a.splice(2,1)
 logA(a); log('\n')
+
+
+//배열 복사하기
+let fruits = ['사과', '바나나']
+let shallowCopy = fruits.slice() // 사본을 만드는 방법
+// ["딸기", "망고"]
+shallowCopy[1] = 'ㄱㄱ'
+logA(fruits)
+
+
+// Array(배열) https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array
+// 여기 부터 읽을 차례: => https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array#%EB%A7%A4%EC%B9%98_%EA%B2%B0%EA%B3%BC%EB%A5%BC_%EC%9D%B4%EC%9A%A9%ED%95%9C_%EB%B0%B0%EC%97%B4_%EC%83%9D%EC%84%B1
+
+
+//생성자 함수 연습
+function Student(name, korean) {
+  this.name = name
+  this.korean = korean
+}
+
