1f5e2c499ddc99a1c7b4a8dff5dffa0a51bbee47
Clark19 <clarkgable1@gmail.com>
2022-08-20 02:43:09 +0900

    feat: 할일 목록 편집/삭제/체크(완료) 기능 구현

--- ToDoList_Wheather/ToDoList.js
+++ ToDoList_Wheather/ToDoList.js
@@ -8,16 +8,97 @@ class App {
   }
 
   #onClickBtnAdd(e) {
-    console.log(this.#inputTodo.value);
+    if (!this.#inputTodo.value || !this.#inputTodo.value.trim()) {
+      this.#inputTodo.value = "";
+      this.#inputTodo.focus();
+      return false;
+    }
 
     const $newLi = document.createElement("li");
-    $newLi.innerText = this.#inputTodo.value;
+    $newLi.appendChild(this.#createCheckBtn());
+    $newLi.appendChild(this.#createEditTextBtn());
+    $newLi.appendChild(this.#createEditBtn());
+    $newLi.appendChild(this.#createDelBtn());
+    $newLi.setAttribute("id", `item-${new Date().getTime()}`);
+
     this.#todoList.insertBefore($newLi, this.#todoList.childNodes[0]);
 
     this.#inputTodo.value = "";
     this.#inputTodo.focus();
 
     return false;
+  }
+
+  #createCheckBtn() {
+    const checkBtn = document.createElement("span");
+    checkBtn.classList.add("check");
+    // checkBtn.innerText = "🔳🔲⬜⬛☑✔✅"; // '&check;'
+    // checkBtn.innerHTML = "&#x2705;"; // '&check;'
+    checkBtn.innerText = "⬜";
+    checkBtn.checked = false;
+    checkBtn.onclick = (e) => {
+      // this.parentNode.classList.toggle("checked");
+      e.currentTarget.parentNode.childNodes[1].classList.toggle("checked");
+      if (!e.currentTarget.checked) {
+        checkBtn.innerText = "✅";
+        checkBtn.checked = true;
+      } else {
+        checkBtn.innerText = "⬜";
+        checkBtn.checked = false;
+      }
+    };
+    return checkBtn;
+  }
+  #createEditTextBtn() {
+    const inputText = document.createElement("input");
+    inputText.value = this.#inputTodo.value;
+    inputText.readOnly = true;
+    inputText.disabled = true;
+
+    return inputText;
+  }
+  #createEditBtn() {
+    const btnEdit = document.createElement("button");
+    btnEdit.appendChild(document.createTextNode("✏")); // \u270F
+    btnEdit.classList.add("btn");
+    btnEdit.isReadOnlyMode = true;
+    btnEdit.onclick = function (e) {
+      if (e.currentTarget.isReadOnlyMode) {
+        this.parentNode.childNodes[1].disabled = false;
+        this.parentNode.childNodes[1].readOnly = false;
+        this.parentNode.childNodes[1].select();
+        this.parentNode.childNodes[2].replaceChild(
+          document.createTextNode("✔"), // \u2714
+          this.parentNode.childNodes[2].firstChild
+        );
+        e.currentTarget.isReadOnlyMode = false;
+      } else {
+        // EDIT_POSSIBLE_MODE
+        this.parentNode.childNodes[1].disabled = true;
+        this.parentNode.childNodes[1].readOnly = true;
+        this.parentNode.childNodes[2].replaceChild(
+          document.createTextNode("✏"), // \u270F
+          this.parentNode.childNodes[2].firstChild
+        );
+        e.currentTarget.isReadOnlyMode = true;
+      }
+    };
+
+    return btnEdit;
+  }
+  #createDelBtn() {
+    const btnDel = document.createElement("button");
+    btnDel.innerText = "❌"; // \u274c
+    const clsAttr = document.createAttribute("class");
+    clsAttr.value = "btn";
+    btnDel.setAttributeNode(clsAttr);
+    btnDel.addEventListener("click", (e) =>
+      e.currentTarget.parentNode.parentNode.removeChild(
+        e.currentTarget.parentNode
+      )
+    );
+
+    return btnDel;
   }
 }
 
